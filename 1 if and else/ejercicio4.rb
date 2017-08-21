42935f0fe20f31ef6ffdbdbb4daa63ac5f84ecc3
Fernanda Vilches <f.vilchesc@gmail.com>
2017-08-21 20:40:22 -0300

    respuestas ejercicios 4 a 6

--- 1 if and else/ejercicio4.rb
+++ 1 if and else/ejercicio4.rb
@@ -1,6 +1,7 @@
-# Permitir que el usuario pueda ingresar la contraseña por teclado 
+# Permitir que el usuario pueda ingresar la contraseña por teclado
 # para que se cumpla la condición.
-
+puts 'Ingresa tu password'
+password = gets.chomp
 if password == 'secreto'
   puts 'Acceso PERMITIDO! :)'
 else
--- 1 if and else/ejercicio6.rb
+++ 1 if and else/ejercicio6.rb
@@ -1,15 +1,9 @@
 # Utiliza álgebra booleana para hacer un refactoring de este código
 # Para verificar la respuestas, puedes modificar los valores de a y b.
 
-a = 'verdadero'
-b = 'falso'
+a = 'falso'
+b = 'verdadero'
 
-if a == 'verdadero'
-    puts ':)'
-else
-  if b == 'verdadero'
-    puts ':|'
-  else
-    puts ':('
-  end
-end
+puts ':)' if a == 'verdadero'
+puts ':|' if b == 'verdadero'
+puts ':(' if a == 'falso' || b == 'falso'
